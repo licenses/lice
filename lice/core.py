0ff6b737049591ac0acdc322072a81c99474b574
Jeremy Carbaugh <jcarbaugh@gmail.com>
2013-01-18 19:47:38 -0500

    add loading from template and --vars option to show which variables a template uses

--- lice/core.py
+++ lice/core.py
@@ -13,18 +13,31 @@ LICENSES = ["apache", "bsd2", "bsd3", "cddl", "cc0", "epl",
 DEFAULT_LICENSE = "bsd3"
 
 
+def clean_path(p):
+    p = os.path.expanduser(p)
+    p = os.path.expandvars(p)
+    p = os.path.abspath(p)
+    return p
+
+
 def guess_organization():
-    
     try:        
         stdout = subprocess.check_output('git config --get user.name'.split())
         org = stdout.strip()
     except OSError:
         org = os.environ["USER"]
-        
     return org
 
 
-def load_template(license):
+def load_file_template(path):
+    if not os.path.exists(path):
+        raise ValueError("path does not exist: %s" % path)
+    with open(clean_path(path)) as infile:
+        template = infile.read()
+    return template
+
+
+def load_package_template(license):
     with resource_stream(__name__, 'template-%s.txt' % license) as licfile:
         content = licfile.read()
     return content
@@ -37,34 +50,59 @@ def extract_vars(template):
     return keys
 
 
-def generate_license(license, context):
-    
-    template = load_template(license)
-    
+def generate_license(template, context):
     for key in extract_vars(template):
-        
         if key not in context:
             raise ValueError("%s is missing from the template context" % key)
-        
         template = template.replace("{{ %s }}" % key, context[key])
-    
     return template
+
 
 def main():
     
     parser = argparse.ArgumentParser(description='Generate a license')
+    
     parser.add_argument('license', metavar='license', nargs="?",
                        help='the license to generate, one of: %s' % ", ".join(LICENSES))
     parser.add_argument('-o', '--org', dest='organization',
                        help='organization, defaults to .gitconfig or os.environ["USER"]')
     parser.add_argument('-p', '--proj', dest='project',
                        help='name of project, defaults to name of current directory')
-    parser.add_argument('-t', '--template', dest='template',
+    parser.add_argument('-t', '--template', dest='template_path',
                        help='path to license template file')
     parser.add_argument('-y', '--year', dest='year',
                        help='copyright year')
+    parser.add_argument('--vars', dest='list_vars', action="store_true",
+                       help='list template variables for specified license')
 
     args = parser.parse_args()
+    
+    # do license stuff
+    
+    license = args.license or DEFAULT_LICENSE
+
+    if license not in LICENSES:
+        parser.error("license must be one of: %s" % ", ".join(LICENSES))
+        
+    # list template vars if requested
+    
+    if args.list_vars:
+        
+        if args.template_path:
+            template = load_file_template(args.template_path)
+        else:
+            template = load_package_template(license)
+        
+        var_list = extract_vars(template)
+            
+        if var_list:
+            sys.stdout.write("The %s license template contains the following variables:\n" % (args.template_path or license))
+            for v in var_list:
+                sys.stdout.write("  %s\n" % v)
+        else:
+            sys.stdout.write("The %s license template contains no variables.\n" % (args.template_path or license))
+        
+        sys.exit(0)
 
     # check year, if specified
         
@@ -79,18 +117,13 @@ def main():
         "project": args.project or os.getcwd().split(os.sep)[-1],
     }
 
-    if args.template:
-        pass
-        
+    if args.template_path:
+        template = load_file_template(args.template_path)
     else:
-
-        license = args.license or DEFAULT_LICENSE
-
-        if license not in LICENSES:
-            parser.error("license must be one of: %s" % ", ".join(LICENSES))
+        template = load_package_template(license)
             
-        content = generate_license(license, context)
-        sys.stdout.write(content)
+    content = generate_license(template, context)
+    sys.stdout.write(content)
 
 
 if __name__ == "__main__":
