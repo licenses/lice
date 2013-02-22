abebf4a17a07806dbf77a6d603f6e8b1cffe39e6
Jeremy Carbaugh <jcarbaugh@gmail.com>
2013-02-22 01:08:22 -0500

    use rst file in setup.py

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from lice.core import __version__
 from setuptools import setup, find_packages
 
-long_description = open('README.md').read()
+long_description = open('README.rst').read()
 
 setup(
     name="lice",
