e6bbbd4dd84ee1031569eebfbb20772e51667cdf
Jeremy Carbaugh <jcarbaugh@gmail.com>
2013-06-03 12:09:00 -0400

    install argparse on 2.6 so travis-ci tests can pass

--- setup.py
+++ setup.py
@@ -7,6 +7,7 @@ long_description = open('README.rst').read()
 extra_kwargs = {}
 if sys.version_info < (2, 7):
     extra_kwargs['setup_requires'] = ['argparse>=1.2', 'unittest2']
+    extra_kwargs['install_requires'] = ['argparse>=1.2']
 if sys.version_info >= (3,):
     extra_kwargs['setup_requires'] = ['setuptools']
 
